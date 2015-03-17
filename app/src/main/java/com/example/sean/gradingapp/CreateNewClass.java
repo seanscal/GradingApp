a16e8d44a0249b7bbb4590f818f324e9a454d877
Sean Scalabrini <sscalabrini@digitallumens.com>
2015-03-17 02:03:06 -0400

    attempts to save after dynamically creating

--- app/src/main/java/com/example/sean/gradingapp/CreateNewClass.java
+++ app/src/main/java/com/example/sean/gradingapp/CreateNewClass.java
@@ -1,16 +1,16 @@
 package com.example.sean.gradingapp;
 
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
-import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RadioGroup;
-import android.widget.Toast;
 
 public class CreateNewClass extends Activity {
 
@@ -19,9 +19,23 @@ public class CreateNewClass extends Activity {
         setContentView(R.layout.create_new);
     }
 
-    public void Submit (View view) {
+    private static final AtomicInteger sNextGeneratedId = new AtomicInteger(1);
 
-        Button myButton = new Button(this);
+    public static int generateViewId() {
+        for (;;) {
+            final int result = sNextGeneratedId.get();
+            // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
+            int newValue = result + 1;
+            if (newValue > 0x00FFFFFF) newValue = 1; // Roll over to 1, not 0.
+            if (sNextGeneratedId.compareAndSet(result, newValue)) {
+                return result;
+            }
+        }
+    }
+    public void Submit (View view) {
+        Intent myIntent = new Intent(CreateNewClass.this, MainActivity.class);
+        Random randomGenerator = new Random();
+        int randomInt = randomGenerator.nextInt(99999999);
         EditText mEdit   = (EditText)findViewById(R.id.class_name);
         String name = mEdit.getText().toString();
 
@@ -31,6 +45,15 @@ public class CreateNewClass extends Activity {
         mEdit   = (EditText)findViewById(R.id.class_year);
         String year = mEdit.getText().toString();
 
+        Button myButton = new Button(this);
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            myButton.setId(generateViewId());
+        }
+        else {
+            myButton.setId(Button.generateViewId());
+        }
+
         myButton.setText(name + " " + semester + " " + year);
 
         setContentView(R.layout.activity_main);
@@ -39,8 +62,6 @@ public class CreateNewClass extends Activity {
                 (LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         ll.addView(myButton, layoutParams);
 
-        Intent myIntent = new Intent(CreateNewClass.this, MainActivity.class);
         startActivity(myIntent);
     }
-
 }
